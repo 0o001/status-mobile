1be8d64d04b0b3cb015af74ca647a0ff0aba2e84
Siddarth Kumar <siddarthkay@gmail.com>
2023-09-03 17:58:21 +0530

    debug: get the app working in debug mode

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,28 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
-  env: {
-    test: {
-      presets: [
-        '@babel/preset-react',
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: '14',
-            },
-          },
-        ],
-      ],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: ['react-native-reanimated/plugin',
+    '@babel/plugin-proposal-logical-assignment-operators'],
+    env: {
+        test: {
+            presets: [
+                '@babel/preset-react',
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            node: '14',
+                        },
+                        // Ensure all ES2021 features are transpiled
+                        // for older environments.
+                        spec: true,
+                    },
+                ],
+            ],
+            // If there are any ES2021 features that require
+            // plugins, you can add them here.
+            plugins: [
+                '@babel/plugin-proposal-logical-assignment-operators',
+            ],
+        },
     },
-  },
 };
